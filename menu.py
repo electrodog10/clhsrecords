f3eeaed48609cd7c8220da19aec0467cbf6328b2
Henry Luker <hluker18@lions.clearlakeschools.org>
2017-01-24 11:27:47 -0600

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,7 @@
+def menu (searchnumber,search):
+    print('Please select an option')
+    print(searchnumber[0], search[0])
+    print(searchnumber[1], search[1])
+    print(searchnumber[2], search[2])
+    print(searchnumber[3], search[3])
+    return
---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print("Content-Type: text/html\n\n")  # html markup follows
+
+print("""
+<html>
+  <Title>Hello in bob</Title>
+<body>
+  <p>Hello There!</p>
+  <p><b>Hi Thbbbe!</b></p>  
+</body>
+</html> """)
